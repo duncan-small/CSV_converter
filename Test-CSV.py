33cbc219dd20d315db1a428f7dddafdb1712bb1e
duncan_small <duncan.small@icloud.com>
2017-07-17 10:36:11 -0400

    Adding sorting base on the columns

--- Test-CSV.py
+++ Test-CSV.py
@@ -1,8 +1,23 @@
 import csv
 final = []
-
+colOne = []
+colTwo = []
+colThree = []
+colFour = []
+colFive = []
+colSix = []
+colSeven = []
+combined = [colOne,colTwo,colThree,colFour,colFive,colSix,colSeven]
+sortDict = {0:colOne,1:colTwo,2:colThree,3:colFour,4:colFive,5:colSix,6:colSeven}
 with open("Testing.csv", newline='') as thing:
-    redy = csv.reader(thing)
-    for x in redy:
-        final.append(x)
-    print (final)
+    baseData = csv.reader(thing)
+    for rows in baseData:
+        while (len(rows) != 7):
+            rows.append('')
+        final.append(rows)
+    for rowNew in range(len(final)):
+        for column in range(len(final[rowNew])):
+            sortDict[column].append(final[rowNew][column])
+    print (combined)
+            
+            
