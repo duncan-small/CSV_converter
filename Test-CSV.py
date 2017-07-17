24471f85795fb77a63918dcb9a93b347dcd28d3a
duncan_small <duncan.small@icloud.com>
2017-07-17 13:56:37 -0400

    Added comments to give more clarity on what the script is doing

--- Test-CSV.py
+++ Test-CSV.py
@@ -9,20 +9,19 @@ colSix = []
 colSeven = []
 combined = [colOne,colTwo,colThree,colFour,colFive,colSix,colSeven]
 sortDict = {0:colOne,1:colTwo,2:colThree,3:colFour,4:colFive,5:colSix,6:colSeven}
-def precinctSep(precRow):
+def precinctSep(precRow): #This function seperates the precinct name from the id, the original format is "id - name", I make it a list : [name,id]
     precRow = precRow.split("-",1)
     for temp in range(len(precRow)):
         precRow[temp] = precRow[temp].strip()
     return precRow
 with open("Testing.csv", newline='') as thing:
     baseData = csv.reader(thing)
-    for rows in baseData:
+    for rows in baseData: #This loop makes the main file a more standard list
         while (len(rows) != 7):
             rows.append('')
         final.append(rows)
     for rowss in range(len(final)):
-        #print (precinctSep(final[rowss][2]))
-        if rowss <= 1 or rowss == (len(final) - 1):
+        if rowss <= 1 or rowss == (len(final) - 1): #These conditions remove useless information
             pass
         elif len(precinctSep(final[rowss][2])) == 2:
             print ("INSERT INTO Precincts VALUES (" , (precinctSep(final[rowss][2]))[1] , "," , (precinctSep(final[rowss][2]))[0] , "," , final[rowss][0] , ")" )
