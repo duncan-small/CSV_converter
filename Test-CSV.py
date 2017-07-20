96fdd9e75ef0145cd97bdabcbc459346fdcaa073
duncan_small <duncan.small@icloud.com>
2017-07-20 14:51:40 -0400

    Removed things that weren't needed and added a layer of filtering to get only District 31 Data

--- Test-CSV.py
+++ Test-CSV.py
@@ -5,18 +5,9 @@ import re
 final = []
 last = []
 
-colOne = []
-colTwo = []
-colThree = []
-colFour = []
-colFive = []
-colSix = []
-colSeven = []
+districtSpecific = ('101 - Kettle Run','102 - Catlett','103 - Casanova','502 - New Baltimore','504 - Vint Hill','201 - Saunders','207 - Spriggs','208 - Hylton','209 - Independent Hill','210 - Penn','305 - Pattie','306 - Washington-reid','307 - Henderson','308 - Montclair','309 - Ashland','310 - Forest Park','602 - Beville','603 - Godwin','605 - Minnieville','608 - Enterprise','609 - King')
 
 pattern = re.compile("\d") #makes a pattern that checks for a digit
-
-combined = [colOne,colTwo,colThree,colFour,colFive,colSix,colSeven]
-sortDict = {0:colOne,1:colTwo,2:colThree,3:colFour,4:colFive,5:colSix,6:colSeven}
 
 def precinctSep(precRow): #This function seperates the precinct name from the id, the original format is "id - name", I make it a list : [name,id]
 
@@ -33,21 +24,22 @@ for root, dirs, filenames in os.walk(indir):
             
             baseData = csv.reader(thing)
             for rows in baseData: #This loop makes the main file a more standard list called 'final'
-
-                while (len(rows) != 7):
-                    rows.append('')
-                rows.append(str(f).replace('Virginia_Elections_Database__','').replace('_including_precincts.csv',''))
-                final.append(rows)
+                if ((rows[0] == 'Fauquier County' or rows[0] == 'Prince William County') and (rows[2].startswith(districtSpecific))):
+                    print(rows[2])
+                    rows.append(str(f).replace('Virginia_Elections_Database__','').replace('_including_precincts.csv',''))
+                    final.append(rows)
                 
     for rowss in range(len(final)): #This loop makes the INSERT statements for the Precinct Table
-
         if len(precinctSep(final[rowss][2])) == 2:
             last.append("INSERT INTO Precincts VALUES ('" + str((precinctSep(final[rowss][2]))[1]) + "' , '" + (precinctSep(final[rowss][2]))[0] + "', '" + final[rowss][0].replace(" ","_") + "', " + str(final[rowss][3]).replace(',','') +", " + str(final[rowss][4]).replace(',','') +", " + str(final[rowss][5]).replace(',','') + ", " + str(final[rowss][6]).replace(',','') + ", '" + final[rowss][7] +"' );" )
+
         elif not (pattern.match(final[rowss][4])): #This condition removes useless information
             pass
-        
+
         else:
             last.append("INSERT INTO Precincts VALUES ( 'NA' , 'NA' , '" + final[rowss][0].replace(" ","_") + "'," + str(final[rowss][3]).replace(',','') +", " + str(final[rowss][4]).replace(',','') +", " + str(final[rowss][5]).replace(',','') +", " + str(final[rowss][6]).replace(',','') + ", '" + final[rowss][7] + "' );" )
+
+
 print( "CREATE TABLE Precincts (PrecinctNames varchar(255), PrecinctID varchar(255), ParentCounty varchar(255), Republican int, Democrat int, OtherVotes int, TotalVotesCast int, FileName varchar(255));")
 for x in last:
     print (x)
